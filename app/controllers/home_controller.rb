8d52c8d080970f925bf7b1a06b4d87ae31b570cf
Galen Eppler <geppler@uccs.edu>
2018-10-08 20:29:13 -0600

    Added coinWallet object to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def trade
   end
+  def getCurrencyExchange(coin1, coin2)
+      coinWallet wallet
+      exchangeRate = wallet.getExchangePrice(coin1) / wallet.getExchangePrice(coin2)
+      return exchangeRate
+  end
 end
